4fdc863a82b22300f0650a38d5d5f6ca8f644672
Minh Hoàng <51706032+TLMHoang@users.noreply.github.com>
2019-12-10 14:42:40 +0700

    lab02_3

--- lab02/lab02_3/Form1.Designer.cs
+++ lab02/lab02_3/Form1.Designer.cs
@@ -37,16 +37,11 @@
             this.txtTKH = new System.Windows.Forms.TextBox();
             this.txtDiaChi = new System.Windows.Forms.TextBox();
             this.txtSoTienTK = new System.Windows.Forms.TextBox();
-            this.btnThem = new System.Windows.Forms.Button();
             this.btnLuu = new System.Windows.Forms.Button();
+            this.btnCapNhap = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnThoat = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lstvData = new System.Windows.Forms.ListView();
             this.label6 = new System.Windows.Forms.Label();
             this.txtTien = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
@@ -55,11 +50,11 @@
             // 
             this.label1.BackColor = System.Drawing.Color.Yellow;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(819, 47);
+            this.label1.Size = new System.Drawing.Size(819, 74);
             this.label1.TabIndex = 0;
             this.label1.Text = "Quản Lý Thông Tin Tài Khoản";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -67,9 +62,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(93, 100);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(91, 100);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(104, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Số Tài Khoản";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -77,9 +73,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(93, 142);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(67, 145);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.Size = new System.Drawing.Size(128, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Tên Khách Hàng";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -87,9 +84,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(93, 192);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(52, 192);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.Size = new System.Drawing.Size(143, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Địa Chỉ Khác Hàng";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -97,67 +95,75 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(93, 238);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(12, 235);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(127, 13);
+            this.label5.Size = new System.Drawing.Size(183, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Số Tiền Trong Tài Khoản";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtSTK
             // 
+            this.txtSTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtSTK.Location = new System.Drawing.Point(226, 97);
             this.txtSTK.Name = "txtSTK";
-            this.txtSTK.Size = new System.Drawing.Size(561, 20);
+            this.txtSTK.Size = new System.Drawing.Size(561, 26);
             this.txtSTK.TabIndex = 5;
-            this.txtSTK.TextChanged += new System.EventHandler(this.txtSTK_TextChanged);
             // 
             // txtTKH
             // 
+            this.txtTKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtTKH.Location = new System.Drawing.Point(226, 142);
             this.txtTKH.Name = "txtTKH";
-            this.txtTKH.Size = new System.Drawing.Size(561, 20);
+            this.txtTKH.Size = new System.Drawing.Size(561, 26);
             this.txtTKH.TabIndex = 6;
             // 
             // txtDiaChi
             // 
+            this.txtDiaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtDiaChi.Location = new System.Drawing.Point(226, 192);
             this.txtDiaChi.Name = "txtDiaChi";
-            this.txtDiaChi.Size = new System.Drawing.Size(561, 20);
+            this.txtDiaChi.Size = new System.Drawing.Size(561, 26);
             this.txtDiaChi.TabIndex = 7;
             // 
             // txtSoTienTK
             // 
+            this.txtSoTienTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtSoTienTK.Location = new System.Drawing.Point(226, 235);
             this.txtSoTienTK.Name = "txtSoTienTK";
-            this.txtSoTienTK.Size = new System.Drawing.Size(561, 20);
+            this.txtSoTienTK.Size = new System.Drawing.Size(561, 26);
             this.txtSoTienTK.TabIndex = 8;
-            // 
-            // btnThem
-            // 
-            this.btnThem.Location = new System.Drawing.Point(226, 270);
-            this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(75, 23);
-            this.btnThem.TabIndex = 9;
-            this.btnThem.Text = "Thêm";
-            this.btnThem.UseVisualStyleBackColor = true;
-            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            this.txtSoTienTK.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoTienTK_KeyPress);
             // 
             // btnLuu
             // 
-            this.btnLuu.Location = new System.Drawing.Point(352, 270);
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnLuu.Location = new System.Drawing.Point(41, 270);
             this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(75, 23);
-            this.btnLuu.TabIndex = 10;
+            this.btnLuu.Size = new System.Drawing.Size(75, 31);
+            this.btnLuu.TabIndex = 9;
             this.btnLuu.Text = "Lưu";
             this.btnLuu.UseVisualStyleBackColor = true;
-            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            this.btnLuu.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnCapNhap
+            // 
+            this.btnCapNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCapNhap.Location = new System.Drawing.Point(122, 270);
+            this.btnCapNhap.Name = "btnCapNhap";
+            this.btnCapNhap.Size = new System.Drawing.Size(105, 31);
+            this.btnCapNhap.TabIndex = 10;
+            this.btnCapNhap.Text = "Cập nhập";
+            this.btnCapNhap.UseVisualStyleBackColor = true;
+            this.btnCapNhap.Click += new System.EventHandler(this.btnLuu_Click);
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(483, 270);
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnXoa.Location = new System.Drawing.Point(233, 270);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.Size = new System.Drawing.Size(75, 31);
             this.btnXoa.TabIndex = 11;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
@@ -165,60 +171,31 @@
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(605, 270);
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThoat.Location = new System.Drawing.Point(314, 270);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.Size = new System.Drawing.Size(75, 31);
             this.btnThoat.TabIndex = 12;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
             this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
-            // listView1
+            // lstvData
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4,
-            this.columnHeader5});
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(96, 323);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(691, 236);
-            this.listView1.TabIndex = 13;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "STT";
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Mã Khách hàng";
-            this.columnHeader2.Width = 120;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Tên Khách Hàng";
-            this.columnHeader3.Width = 126;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Địa Chỉ";
-            this.columnHeader4.Width = 154;
-            // 
-            // columnHeader5
-            // 
-            this.columnHeader5.Text = "Số Tiền";
-            this.columnHeader5.Width = 273;
+            this.lstvData.HideSelection = false;
+            this.lstvData.Location = new System.Drawing.Point(12, 307);
+            this.lstvData.Name = "lstvData";
+            this.lstvData.Size = new System.Drawing.Size(800, 275);
+            this.lstvData.TabIndex = 13;
+            this.lstvData.UseCompatibleStateImageBehavior = false;
+            this.lstvData.View = System.Windows.Forms.View.Details;
+            this.lstvData.SelectedIndexChanged += new System.EventHandler(this.lstvData_SelectedIndexChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(507, 565);
+            this.label6.Location = new System.Drawing.Point(517, 272);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(107, 24);
             this.label6.TabIndex = 14;
@@ -226,10 +203,13 @@
             // 
             // txtTien
             // 
-            this.txtTien.Location = new System.Drawing.Point(630, 565);
+            this.txtTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtTien.Location = new System.Drawing.Point(630, 272);
             this.txtTien.Name = "txtTien";
-            this.txtTien.Size = new System.Drawing.Size(157, 20);
+            this.txtTien.ReadOnly = true;
+            this.txtTien.Size = new System.Drawing.Size(157, 26);
             this.txtTien.TabIndex = 15;
+            this.txtTien.Text = "0";
             // 
             // Form1
             // 
@@ -238,11 +218,11 @@
             this.ClientSize = new System.Drawing.Size(819, 594);
             this.Controls.Add(this.txtTien);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lstvData);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnCapNhap);
             this.Controls.Add(this.btnLuu);
-            this.Controls.Add(this.btnThem);
             this.Controls.Add(this.txtSoTienTK);
             this.Controls.Add(this.txtDiaChi);
             this.Controls.Add(this.txtTKH);
@@ -270,16 +250,11 @@
         private System.Windows.Forms.TextBox txtTKH;
         private System.Windows.Forms.TextBox txtDiaChi;
         private System.Windows.Forms.TextBox txtSoTienTK;
-        private System.Windows.Forms.Button btnThem;
         private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnCapNhap;
         private System.Windows.Forms.Button btnXoa;
         private System.Windows.Forms.Button btnThoat;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ColumnHeader columnHeader3;
-        private System.Windows.Forms.ColumnHeader columnHeader4;
-        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ListView lstvData;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtTien;
     }
---
+++ lab02/lab02_3/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab02_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TaoBang();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        void TongTien()
+        {
+            txtTien.Text = "0";
+            txtSTK.Text = "";
+            txtTKH.Text = "";
+            txtDiaChi.Text = "";
+            txtSoTienTK.Text = "";
+            foreach (ListViewItem i in lstvData.Items)
+            {
+
+                txtTien.Text = (int.Parse(txtTien.Text) + int.Parse(i.SubItems[4].Text)).ToString();
+            }
+            
+        }
+
+        private void TaoBang()
+        {
+            lstvData.Columns.Add("STT", 50);
+            lstvData.Columns.Add("Mã KH", 150);
+            lstvData.Columns.Add("Tên KH", 150);
+            lstvData.Columns.Add("Địa chỉ", 320);
+            lstvData.Columns.Add("Tiền", 170);
+            lstvData.FullRowSelect = true;
+            lstvData.GridLines = true;
+        }
+
+        private bool CheckNull()
+        {
+            if (txtSTK.Text == "")
+            {
+                MessageBox.Show("Chưa nhập số tài khoản");
+                return false;
+            }
+            if (txtTKH.Text == "")
+            {
+                MessageBox.Show("Chưa nhập tên khách hàng");
+                return false;
+            }
+            if (txtDiaChi.Text == "")
+            {
+                MessageBox.Show("Chưa nhập địa chỉ");
+                return false;
+            }
+            if (txtSoTienTK.Text == "")
+            {
+                MessageBox.Show("Chưa nhập Số tiền trong tài khoản");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private ListViewItem AddData()
+        {
+            ListViewItem item = new ListViewItem((lstvData.Items.Count + 1).ToString());
+            item.SubItems.Add(txtSTK.Text);
+            item.SubItems.Add(txtTKH.Text);
+            item.SubItems.Add(txtDiaChi.Text);
+            item.SubItems.Add(txtSoTienTK.Text);
+
+            return item;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (CheckNull())
+            {
+                Thread t = new Thread(TongTien);
+                t.Start();
+                lstvData.Items.Add(AddData());
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bạn có muốn xóa không","",MessageBoxButtons.OKCancel)== DialogResult.OK)
+            {
+                foreach (ListViewItem item in lstvData.SelectedItems)
+                    lstvData.Items.Remove(item);
+                
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (CheckNull())
+            {
+                lstvData.SelectedItems[0].SubItems[1].Text = txtSTK.Text;
+                lstvData.SelectedItems[0].SubItems[2].Text = txtTKH.Text;
+                lstvData.SelectedItems[0].SubItems[3].Text = txtDiaChi.Text;
+                lstvData.SelectedItems[0].SubItems[4].Text = txtSoTienTK.Text;
+                Thread t = new Thread(TongTien);
+                t.Start();
+            }
+            
+
+        }
+
+        private void txtSoTienTK_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != (char)Keys.Back)
+            {
+                if (!char.IsDigit(e.KeyChar))
+                {
+                    e.KeyChar = (char)Keys.Back;
+                    MessageBox.Show("Ô này chỉ có thể nhập số không nhập được chữ");
+                }
+            }
+            
+        }
+
+        private void lstvData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(LoadInfo);
+            thread.Start();
+        }
+
+        void LoadInfo()
+        {
+            if (lstvData.SelectedItems.Count > 0)
+            {
+                txtSTK.Text = lstvData.SelectedItems[0].SubItems[1].Text;
+                txtTKH.Text = lstvData.SelectedItems[0].SubItems[2].Text;
+                txtDiaChi.Text = lstvData.SelectedItems[0].SubItems[3].Text;
+                txtSoTienTK.Text = lstvData.SelectedItems[0].SubItems[4].Text;
+            }
+        }
+    }
+}
